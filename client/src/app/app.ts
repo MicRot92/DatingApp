3f69bdb23350bf862e31bf22b812fd8751d7340f
Michal Rotynski <mrotynski@gmail.com>
2026-01-21 19:53:23 +0100

    Cleanup app.ts from unused accountService

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, Signal, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../Types/user';
 import { RouterOutlet } from '@angular/router';
 
@@ -15,18 +13,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   protected readonly title = signal('Dating App');
   protected members = signal<User[]>([]);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
-    this.setCurrentUser();
-
-  }
-
-  setCurrentUser() {
-
   }
 
   async getMembers() {
