41b8e350154af9735a87479d877b432b8483b1ae
Michal Rotynski <mrotynski@gmail.com>
2026-01-21 02:18:13 +0100

    Add toast for login functionality

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -11,23 +12,30 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 })
 export class Nav {
   protected account = inject(AccountService);
-  protected creds: any = {};
+  private router = inject(Router)
+  private toast = inject(ToastService);
 
+  protected creds: any = {};
   login() {
     this.account.login(this.creds).subscribe({
       next: (response: any) => {
+        this.router.navigateByUrl('/members');
         console.log('Login successful:', response);
+        this.toast.success('Login successful');
         this.account.login(this.creds);
         this.creds = {};
       },
       error: (error: any) => {
         console.error('Login failed:', error);
+        this.toast.error('Login failed: ' + error.error);
+        this.router.navigateByUrl('/');
       }
     });
   }
 
   logout() {
     this.account.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
