614553471867d05d700e397874c0518b2f4e7fa9
Michal Rotynski <mrotynski@gmail.com>
2026-01-22 02:42:54 +0100

    Added Member and Photos Entities and migration

---
+++ API/Entities/Member.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Humanizer;
+
+namespace API.Entities;
+
+public class Member
+{
+    public string Id { get; set; } = null!;
+    public DateOnly DateOfBirth { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public required string DisplayName { get; set; }
+
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+    public required string Gender { get; set; }
+
+    public string? Desciprtion { get; set; }
+
+    public required string City { get; set; }
+
+    public required string Country { get; set; }
+
+    [ForeignKey(nameof(Id))]
+    public AppUser User { get; set; } = null!;
+
+}
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; } = null!;
+
+    public string? PublicId { get; set; }
+
+    public Member Member { get; set; } = null!;
+}
