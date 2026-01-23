b863295a86be8ea59f7f349bfdb8ad9e5fe3d3ec
Michal Rotynski <mrotynski@gmail.com>
2026-01-23 17:22:30 +0100

    Show member list, add member-card component

--- client/src/Types/user.ts
+++ client/src/Types/user.ts
@@ -2,7 +2,7 @@ export type User = {
     id: string;
     displayName: string;
     token: string;
-    imageUrl?: string;
+    photoUrl?: string;
 };
 
 export type LoginCreds = {
---
+++ client/src/core/services/member-service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../../Types/member';
+import { AccountService } from './account-service';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  private http = inject(HttpClient);
+  private accountService = inject(AccountService);
+  private baseUrl = environment.apiUrl;
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions());
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'members/' + username, this.getHttpOptions());
+  }
+
+  private getHttpOptions() {
+
+    return {
+      headers: {
+        Authorization: 'Bearer ' + this.accountService.currentUser()?.token
+      }
+    };
+
+  }
+} 
