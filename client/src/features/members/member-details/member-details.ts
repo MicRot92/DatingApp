dc53b071058aff627a4d7099f2e1a18f4d71769d
Michal Rotynski <mrotynski@gmail.com>
2026-01-23 19:57:58 +0100

    Add Navigation to Profile, Photos and Messages components on MemeberDetails page

--- client/src/features/members/member-details/member-details.ts
+++ client/src/features/members/member-details/member-details.ts
@@ -1,24 +1,34 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
-import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { filter, Observable } from 'rxjs';
 import { Member } from '../../../Types/member';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-details',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-details.html',
   styleUrl: './member-details.css',
 })
 export class MemberDetails implements OnInit {
   private membersService = inject(MemberService);
   private route = inject(ActivatedRoute);
-
+  private router = inject(Router);
   protected member$?: Observable<Member>;
+  protected title = signal<string | undefined>('Profile');
 
   ngOnInit(): void {
     this.member$ = this.loadMember();
+    this.title.set(this.route.firstChild?.snapshot?.title);
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe({
+
+      next: () => {
+        this.title.set(this.route.firstChild?.snapshot?.title);
+      }
+    })
   }
 
   loadMember() {
