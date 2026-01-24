d876a4f9def8aa49d2bbd4f7a026aebd46e15f52
Michal Rotynski <mrotynski@gmail.com>
2026-01-24 22:58:18 +0100

    Fix the age pipe return value

--- client/src/core/pipes/age-pipe.ts
+++ client/src/core/pipes/age-pipe.ts
@@ -13,8 +13,8 @@ export class AgePipe implements PipeTransform {
     const monthDiff = today.getMonth() - dob.getMonth();
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
       age--;
-      return age;
     }
+    return age;
   }
 
 }
