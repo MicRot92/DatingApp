43ae80c39be52c89eba467c1270d4b75d8b2edc4
Michal Rotynski <mrotynski@gmail.com>
2026-01-28 21:35:35 +0100

    Add fallback controller

--- API/Program.cs
+++ API/Program.cs
@@ -50,7 +50,13 @@ app.UseCors(policy =>
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+
 app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
