5992fe8dbba1e3f3f43ebfd7701ab9a80af1201d
Michal Rotynski <mrotynski@gmail.com>
2026-01-30 02:50:16 +0100

    Extend the registration with additional details and make the registation process 2 step

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,4 +9,13 @@ public class RegisterDto
     [Required]
     [MinLength(6)]
     public string Password { get; set; } = "";
+    [Required]
+    public string Gender { get; set; } = String.Empty;
+    [Required]
+    public DateOnly DateOfBirth { get; set; }
+    [Required]
+    public string City { get; set; } = String.Empty;
+    [Required]
+    public string Country { get; set; } = String.Empty;
+
 }
--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,9 +1,11 @@
-import { Component, inject, input, OnInit, output } from '@angular/core';
+import { Component, inject, input, OnInit, output, signal } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds, User } from '../../../Types/user';
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
 import { TextInput } from "../../../shared/text-input/text-input";
+import { count } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -18,17 +20,20 @@ import { TextInput } from "../../../shared/text-input/text-input";
 })
 export class Register {
   private accountService = inject(AccountService);
+  private router = inject(Router);
   private fb = inject(FormBuilder)
   canceledRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
-  protected registerForm: FormGroup;;
-
+  protected credentialsForm: FormGroup;
+  protected profileForm!: FormGroup;
+  protected currentStep = signal(1);
+  protected validationErrors = signal<string[]>([]);
   /**
    *
    */
   constructor() {
-    this.registerForm = this.fb.group({
-      username: ['', [Validators.required]],
+    this.credentialsForm = this.fb.group({
+      displayName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
       confirmPassword: ['', [
@@ -36,9 +41,17 @@ export class Register {
         this.matchValues('password')
       ]]
     });
-    this.registerForm.controls['password'].valueChanges.subscribe(() => {
-      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    this.credentialsForm.controls['password'].valueChanges.subscribe(() => {
+      this.credentialsForm.controls['confirmPassword'].updateValueAndValidity();
     });
+
+    this.profileForm = this.fb.group({
+      dateOfBirth: [''],
+      city: [''],
+      gender: [''],
+      country: ['']
+    });
+
   }
 
 
@@ -50,17 +63,52 @@ export class Register {
         : { passwordMismatched: true };
     }
   }
+
+  nextStep() {
+    if (this.credentialsForm.valid) {
+      this.currentStep.update(prevStep => prevStep + 1);
+    }
+  }
+
+  prevStep() {
+    this.currentStep.update(prevStep => prevStep - 1);
+  }
+
+  getMaxDate() {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    return today.toISOString().split('T')[0];
+  }
+
   register() {
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.creds).subscribe({
-    //   next: (response: any) => {
-    //     console.log('Registration successful:', response);
-    //     this.cancel();
-    //   },
-    //   error: (error: any) => {
-    //     console.error('Registration failed:', error);
-    //   }
-    // });
+    if (this.credentialsForm.valid && this.profileForm.valid) {
+      const credentials = this.credentialsForm.value;
+      const profile = this.profileForm.value;
+
+      const formData: RegisterCreds = {
+        displayName: credentials.displayName,
+        email: credentials.email,
+        password: credentials.password,
+        gender: profile.gender,
+        dateOfBirth: profile.dateOfBirth,
+        city: profile.city,
+        country: profile.country
+      };
+      console.log('Registering user with data:', formData);
+
+      this.accountService.register(formData).subscribe({
+        next: (response: any) => {
+          console.log('Registration successful:', response);
+          this.router.navigateByUrl('/members');
+          this.cancel();
+        },
+        error: (error: any) => {
+          console.error('Registration failed:', error);
+          this.validationErrors.set(error);
+        }
+      });
+    }
+
   }
 
   cancel() {
